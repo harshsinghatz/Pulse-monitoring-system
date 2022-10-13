6057d160d915c4155795ea31d167e5fd007527c7
Harsh Singh <51085015+hs9085298@users.noreply.github.com>
2022-10-13 09:24:59 +0530

    add: charts and get data from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { pulseRef } from "./firebaseConfig";
+import { get } from "firebase/database";
+import "./App.css";
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Health Monitoring IOT project',
+    },
+  },
+};
 
 function App() {
+  const [data, setData] = useState([]);
+  const chartData = {
+    labels: ["first", "second", "third", "fourth", "fifth"],
+    datasets:[{
+        label: "Pulse Monitoring",
+        data: data.slice(data.length-5, data.length),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#72afff",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ]
+  };
+
+  useEffect(() => {
+      get(pulseRef)
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const res = snapshot.val();
+            setData(Object.values(res));
+          } else {
+            console.log("No data available");
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="heading">
+        <img src="Eo_circle_green_heart.svg.png" alt="heart" className="heart"/>
+      <h1>Health Monitoring System</h1>
+      </div>
+      
+      <div className="circle">
+        <h2>{data.at(-1) ?? "Loading..."}</h2>
+      </div>
+      <div className="chart-container">
+      <Bar data={chartData} options={options}/>
+      </div>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child } from "firebase/database";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const dbRef = ref(db);
+export const pulseRef = child(dbRef, `Pulse`);
